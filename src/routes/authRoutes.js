8e60eaa130bc00b62c74cb4afeae57c31777648e
koglesby <kevinjoglesby@gmail.com>
2021-01-12 16:23:52 -0800

    fix password hashing and add signin route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,16 +4,39 @@ const User = mongoose.model("User");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
+require("dotenv").config();
+const SECRET_KEY = process.env.SECRET_KEY;
+
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = new User({ email, password });
     await user.save();
-    const token = jwt.sign({ userId: user._id }, "SECRET_KEY");
+    const token = jwt.sign({ userId: user._id }, SECRET_KEY);
     res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, SECRET_KEY);
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
   }
 });
 
